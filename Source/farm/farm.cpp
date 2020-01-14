c6d62755729c9affdc24cd17869f84d0db94188e
unknown <pau@students.salle.url.edu>
2020-01-14 12:53:21 +0100

    Added animation and particles. Corrected some  things.

---
+++ Source/farm/farm.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "farm.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, farm, "farm" );
